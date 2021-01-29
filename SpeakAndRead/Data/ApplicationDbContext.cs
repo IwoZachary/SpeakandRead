b7cd40b0d4609835a0b34363804ee4e24274637f
Iwo Zachariasiewicz <iwozachary@gmail.com>
2021-01-29 16:02:14 +0100

    little fix

---
+++ SpeakAndRead/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SpeakAndRead.Models;
+using Microsoft.AspNetCore.Identity;
+namespace SpeakAndRead.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<AppUser> AppUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // create tables for Identity
+        }
+    }
+}
